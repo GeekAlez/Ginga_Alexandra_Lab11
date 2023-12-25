3aaeb0104fe78df2482b7700d4e01153bbdce35f
Ginga Alexandra <alexandra.ginga@stud.ubbcluj.ro>
2023-12-25 23:11:09 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ginga_Alexandra_Lab11.Models;
+
+namespace Ginga_Alexandra_Lab11.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ginga_Alexandra_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
